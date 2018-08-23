e086cd33d393d3d22aef572dc4d7f37fc776c366
limonpudding <martyshenko.sergei@yandex.ru>
2018-08-23 10:14:03 +0500

    Добавлен класс Page

---
+++ src/main/java/app/pagesLogic/Page.java
@@ -0,0 +1,18 @@
+package app.pagesLogic;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+public abstract class Page {
+    public Map<String, Object> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, Object> params) {
+        this.params = params;
+    }
+
+    Map<String, Object> params;
+    public abstract ModelAndView build() throws Exception;
+}
