d3ec0249602861c0e81081c764a5960fa350384b
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-24 12:46:09 +0500

    + Transaction

--- src/main/java/app/config/Config.java
+++ src/main/java/app/config/Config.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
